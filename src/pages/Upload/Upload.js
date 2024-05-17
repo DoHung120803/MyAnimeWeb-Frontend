9abffbbb7dfab1b6dfb85e113dd608879f99e84b
DoHung120803 <y*********@gmail.com>
2024-05-17 07:17:54 +0700

    create form submit to create anime into db in upload page

--- src/pages/Upload/Upload.js
+++ src/pages/Upload/Upload.js
@@ -6,25 +6,52 @@ const cx = classNames.bind(styles);
 function Upload() {
     return (
         <div>
-            <form>
-                <label for="email">Email address</label>
+            <form
+                action="http://localhost:8080/myanime/upload/store"
+                method="POST"
+                enctype="application/x-www-form-urlencoded"
+            >
+                <label className={cx("label")} for="name">
+                    Name
+                </label>
                 <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    placeholder="Email address"
+                    className={cx("input")}
+                    id="name"
+                    name="name"
+                    placeholder="Name..."
                 />
-                <p>We'll never share your email with anyone else.</p>
-                <label for="password">Password</label>
+                <label className={cx("label")} for="description">
+                    Description
+                </label>
                 <input
-                    type="password"
-                    id="password"
-                    name="password"
-                    placeholder="Password"
+                    className={cx("input")}
+                    id="description"
+                    name="description"
+                    placeholder="Description"
                 />
-                <input type="checkbox" id="check-me-out" name="check-me-out" />
-                <label for="check-me-out">Check me out</label>
-                <input type="submit" value="Submit" />
+                <label className={cx("label")} for="iframe">
+                    Iframe
+                </label>
+                <input
+                    className={cx("input")}
+                    id="iframe"
+                    name="iframe"
+                    placeholder="Iframe..."
+                />
+                <label className={cx("label")} for="thumbnail">
+                    Thumbnail
+                </label>
+                <input
+                    className={cx("input")}
+                    id="thumbnail"
+                    name="thumbnailUrl"
+                    placeholder="Thumbnail..."
+                />
+                <input
+                    className={cx("submit-btn")}
+                    type="submit"
+                    value="Submit"
+                />
             </form>
         </div>
     );
